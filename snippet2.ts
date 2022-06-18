3c39e062a3531ab7a5fd906754bacece8475c37a
timon0596 <38828705+timon0596@users.noreply.github.com>
2022-06-18 16:32:15 +0300

    Create snippet2.ts

---
+++ snippet2.ts
@@ -0,0 +1,25 @@
+type NestedKeyOf<ObjectType extends object> = 
+{[Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object 
+? `${Key}` | `${Key}.${NestedKeyOf<ObjectType[Key]>}`
+: `${Key}`
+}[keyof ObjectType & (string | number)];
+  
+type 1NestedKeyOf<ObjectType extends object> = 
+{[Key in keyof ObjectType]: ObjectType[Key] extends object 
+? NestedKeyOf<ObjectType[Key]>
+: Key
+}[keyof ObjectType];
+
+// 2 - Concat the previous key to the path
+type 2NestedKeyOf<ObjectType extends object> = 
+{[Key in keyof ObjectType]: ObjectType[Key] extends object 
+? `${Key}.${NestedKeyOf<ObjectType[Key]>}`
+: Key
+}[keyof ObjectType];
+
+// 3 - Add the object's key
+type 3NestedKeyOf<ObjectType extends object> = 
+{[Key in keyof ObjectType]: ObjectType[Key] extends object 
+? `${Key}` | `${Key}.${NestedKeyOf<ObjectType[Key]>}`
+: Key
+}[keyof ObjectType];
